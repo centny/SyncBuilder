5da8f4170d7fd8bdabbbb2e8650c4b353b8db903
Centny <Scorpion@Wens-MacBook-Pro.local>
2013-04-27 12:17:49 +0800

    adding release builder file

---
+++ ReleaseService/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -lsqlite3 -llog4cxx -lboost_filesystem-mt -lboost_regex -lboost_date_time -lcurl -lextc -lboost_system -lboost_thread-mt
+
