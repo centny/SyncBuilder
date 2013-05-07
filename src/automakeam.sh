1b5beac309e798d853ecff4e7074c10db9cabbaa
Centny <Scorpion@Wens-MacBook-Pro.local>
2013-05-07 17:03:28 +0800

    fix builder prameter.

--- src/automakeam.sh
+++ src/automakeam.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sfs=`find . -name '*.cpp'`
 echo >Makefile.am
-echo AM_CXXFLAGS=-DUSING_LOG4CXX=1 -L/opt/local/lib -I/opt/local/include>>Makefile.am
+echo AM_CXXFLAGS=-DUSING_LOG4CXX=1 -DDEV_NO_SERVICE=0 -L/opt/local/lib -I/opt/local/include>>Makefile.am
 echo bin_PROGRAMS=SyncBuilder>>Makefile.am
 echo "LIBS=\$(SQLITE3_LDFLAGS) \$(BOOST_DATE_TIME_LIB) \$(BOOST_FILESYSTEM_LIB) \$(BOOST_REGEX_LIB) \$(BOOST_THREAD_LIB) \$(BOOST_SYSTEM_LIB) \$(LOG4CXX_LIBS) \$(EXTC_LDFLAGS) \$(LOG4CXX_LDFLAGS) -lcurl">>Makefile.am
 echo SyncBuilder_SOURCES=$sfs>>Makefile.am
