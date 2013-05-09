1bfd76249a89b7a297fdcb2fdadd05ff6f082666
Centny <Scorpion@Wens-MacBook-Pro.local>
2013-05-09 12:15:39 +0800

    update script.

--- src/clean.sh
+++ src/clean.sh
@@ -1,2 +1,2 @@
-make clean
-rm src/depcomp src/ChangeLog src/config.guess src/config.h.in src/config.log src/config.status src/config.sub src/configure src/COPYING src/depcomp src/INSTALL src/install-sh src/Makefile src/Makefile.am src/Makefile.in src/missing src/NEWS src/README src/stamp-h1 src/autom4te.cache src/m4/m4_ax_boost_filesystem.m4  *.Po  src/AUTHORS  src/aclocal.m4  src/config.h  *.o  src/SyncBuilder
+make clean 
+rm depcomp ChangeLog config.guess config.h.in config.log config.status config.sub configure COPYING depcomp INSTALL install-sh Makefile Makefile.am Makefile.in missing NEWS README stamp-h1 autom4te.cache m4/m4_ax_boost_filesystem.m4  *.Po  AUTHORS  aclocal.m4  config.h  *.o  SyncBuilder
